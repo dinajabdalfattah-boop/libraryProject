ae05e12ee159a3297069fcefe2f827b436eb71ce
aeshamajid <aesha.odeh8.3@gmsil.com>
2025-10-06 16:56:31 +0300

    test #2

--- src/main/java/library.java
+++ src/main/java/library.java
@@ -2,6 +2,9 @@ public class library {
     public static int add (int x,int y){
         return x+y;
     }
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
    public  static  void main(String []args ){
        System.out.println("Hello, world!");
 
