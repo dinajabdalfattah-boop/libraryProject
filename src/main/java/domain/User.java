a98cb0c4de817195655b9edc26e5fbc5596efd4f
dinajabdalfattah <dinajabdalfattah@gmail.com>
2025-10-27 12:06:49 +0200

    add classes and package

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -14,29 +14,86 @@ public class User {
         this.borrowedBooks = new ArrayList<>();
     }
 
-    public String getName() { return name; }
-    public double getFineBalance() { return fineBalance; }
-    public void setFineBalance(double fineBalance) { this.fineBalance = fineBalance; }
-    public List<Book> getBorrowedBooks() { return borrowedBooks; }
+    public String getName() {
+        return name;
+    }
 
-    // Borrow a book with rules
+    public String getUserName() {
+        return name;
+    }
+
+    public double getFineBalance() {
+        return fineBalance;
+    }
+
+    public void setFineBalance(double fineBalance) {
+        this.fineBalance = fineBalance;
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    /**
+     * Borrow a book with rules:
+     * - Cannot borrow if user has unpaid fines
+     * - Cannot borrow if user has overdue books
+     */
     public void borrowBook(Book book) {
-        if (fineBalance > 0) throw new IllegalStateException("Pay fines first");
-        for (Book b : borrowedBooks) {
-            if (b.isOverdue()) throw new IllegalStateException("Return overdue books first");
+        if (fineBalance > 0) {
+            throw new IllegalStateException("Cannot borrow: Pay fines first!");
         }
+
+        for (Book b : borrowedBooks) {
+            if (b.isOverdue()) {
+                throw new IllegalStateException("Cannot borrow: Return overdue books first!");
+            }
+        }
+
         book.borrowBook();
         borrowedBooks.add(book);
     }
 
+    /**
+     * Return a borrowed book
+     */
     public void returnBook(Book book) {
         if (borrowedBooks.remove(book)) {
             book.returnBook();
         }
     }
 
+    /**
+     * Pay fines partially or fully
+     */
     public void payFine(double amount) {
-        if (amount > fineBalance) fineBalance = 0;
-        else fineBalance -= amount;
+        if (amount >= fineBalance) {
+            fineBalance = 0;
+        } else {
+            fineBalance -= amount;
+        }
+    }
+
+    /**
+     * Count how many borrowed books are overdue
+     */
+    public int getOverdueCount() {
+        int count = 0;
+        for (Book book : borrowedBooks) {
+            if (book.isOverdue()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Check if the user can be unregistered:
+     * - No active loans
+     * - No unpaid fines
+     */
+    public boolean canBeUnregistered() {
+        boolean hasActiveLoans = borrowedBooks.stream().anyMatch(book -> !book.isAvailable());
+        return !hasActiveLoans && fineBalance <= 0;
     }
 }
